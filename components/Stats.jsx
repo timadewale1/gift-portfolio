50bb3ccda775f9a1bbddea8903ca1462e681c161
timadewale1 <timadewale1@gmail.com>
2025-03-12 07:03:28 +0100

    main

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -32,14 +32,10 @@ const Stats = () => {
                 className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
-                {" "}
-                <CountUp
-                  end={item.num}
-                  duration={5}
-                  delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold"
-                />
-                <span className="text-4xl xl:text-6xl font-extrabold">+</span>
+                <div className="text-4xl xl:text-6xl font-extrabold flex items-baseline">
+                  <CountUp end={item.num} duration={5} delay={2} />
+                  <span className="text-2xl xl:text-4xl">+</span>
+                </div>
                 <p
                   className={`${
                     item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
